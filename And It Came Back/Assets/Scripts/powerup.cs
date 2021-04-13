e05bb5fef6fa9284aa76a0f011c5223e1f75623b
Firefight53 <firefight530@gmail.com>
2021-04-13 16:25:19 -0500

    Common Sprite Sheets
    What that said

--- And It Came Back/Assets/Scripts/powerup.cs
+++ And It Came Back/Assets/Scripts/powerup.cs
@@ -2,24 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class powerup : MonoBehaviour
+public class Powerup : MonoBehaviour
 {
-    public enum PowerupType { Triple, Auto, Bomb, Laser, Shield, Life }
+    //The types that a powerup can be; NA is 'not applicable' and should only be used outside of the powerup itself
+    public enum PowerupType { Triple, Auto, Bomb, Laser, Shield, Life, NA }
     public GameObject bulletPrefab;
 
     public PowerupType type;
     public float tripleSpread = 30f;    //How much the triple spreads in degrees
 
-    public void activate(GameObject bullet)
+    //Activate the powerup on the given object
+    public void activate(GameObject obj)
     {
         Debug.Log("Powerup hit (type=" + type.ToString() + ")");
         switch (type)
         {
             case PowerupType.Triple:
-                activateTriple(bullet);
+                activateTriple(obj);
                 break;
             case PowerupType.Auto:
-                activateAuto(bullet);
+                activateAuto(obj);
                 break;
             case PowerupType.Bomb:
                 //activate...
@@ -40,29 +42,43 @@ public class powerup : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    private void activateTriple(GameObject bullet)
+    private void activateTriple(GameObject obj)
     {
-        //Find the vector perpendicular to the bullet's movement
-        Vector3 velocity = bullet.GetComponent<Rigidbody2D>().velocity;
-        Vector3 perpendicular = Vector3.Cross(velocity, Vector3.back).normalized * .5f;
+        //Activate the powerup on a player
+        if (obj.tag == "Player") {
+            
+        }
+        //Activate the powerup on a bullet
+        else {
+            //Find the vector perpendicular to the bullet's movement
+            Vector3 velocity = obj.GetComponent<Rigidbody2D>().velocity;
+            Vector3 perpendicular = Vector3.Cross(velocity, Vector3.back).normalized * .5f;
 
-        //Create a new bullet angling out from the right side
-        GameObject rightBullet = Instantiate(bulletPrefab);
-        rightBullet.transform.position = bullet.transform.position + perpendicular;
-        rightBullet.GetComponent<Rigidbody2D>().AddForce(
-            Quaternion.Euler(0f, 0f, tripleSpread) * velocity,
-            ForceMode2D.Impulse);
+            //Create a new bullet angling out from the right side
+            GameObject rightBullet = Instantiate(bulletPrefab);
+            rightBullet.transform.position = obj.transform.position + perpendicular;
+            rightBullet.GetComponent<Rigidbody2D>().AddForce(
+                Quaternion.Euler(0f, 0f, tripleSpread) * velocity,
+                ForceMode2D.Impulse);
 
-        //Repeat for the left side
-        GameObject leftBullet = Instantiate(bulletPrefab);
-        leftBullet.transform.position = bullet.transform.position - perpendicular;
-        leftBullet.GetComponent<Rigidbody2D>().AddForce(
-            Quaternion.Euler(0f, 0f, -tripleSpread) * velocity,
-            ForceMode2D.Impulse);
+            //Repeat for the left side
+            GameObject leftBullet = Instantiate(bulletPrefab);
+            leftBullet.transform.position = obj.transform.position - perpendicular;
+            leftBullet.GetComponent<Rigidbody2D>().AddForce(
+                Quaternion.Euler(0f, 0f, -tripleSpread) * velocity,
+                ForceMode2D.Impulse);
+        }
     }
 
-    private void activateAuto(GameObject bullet)
+    private void activateAuto(GameObject obj)
     {
-        bullet.GetComponent<Rigidbody2D>().velocity *= 2f;
+        //Activate the powerup on a player
+        if (obj.tag == "Player") {
+
+        }
+        //Activate the powerup on a bullet
+        else {
+            obj.GetComponent<BulletBehavior>().multSpeed(2f);
+        }
     }
 }
